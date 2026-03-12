2a824c97173dd55076802f64819c5ca0238b460d
minagawargious <minagawargious@utexas.edu>
2026-03-12 15:24:19 -0500

    Display form validation errors and add login page code

--- flask_blog/flask_blog.py
+++ flask_blog/flask_blog.py
@@ -42,10 +42,17 @@ def registration():
         return redirect(url_for("home"))
     return render_template("register.html", title="Register", form=form) # We have access to title and form inside our template
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     # create instance of form we will send to our application
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            # simulate successful login
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("log in unsuccessful", "danger")
     return render_template("login.html", title="Login", form=form) # We have access to title and form inside our template
 
 # Before we run app, we need to set environment variable to file we want to be our flask application. Here, export FLASK_APP=flask_blog.py
