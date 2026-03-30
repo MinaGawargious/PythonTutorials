ad5fe92532c9dab3e191f08c07aaf634f7d0d83e
minagawargious <minagawargious@utexas.edu>
2026-03-30 17:28:41 -0500

    Allow User to update username, email, and profile pic. Resize profile pic to save space

--- flask_blog/flask_blog/forms.py
+++ flask_blog/flask_blog/forms.py
@@ -1,8 +1,10 @@
 # Forms and User Input are so common, we don't have to reinvent the wheel. WT-Forms is available for use
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed # Allow uploading files. FileAllowed is a validator to choose which files are allowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_blog.models import User
+from flask_login import current_user
 # We write Python classes that are representative of our forms, and they will be automatically converted to HTML forms within our template
 
 class RegistrationForm(FlaskForm):
@@ -36,3 +38,23 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     
     submit = SubmitField("Login")
+    
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username Here", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField("Update Profile Picture", validators = [FileAllowed(["jpg", "jpeg", "png"])])
+    
+    submit = SubmitField("Update")
+    
+    # Registration form checked if username or email exists, and throwing ValidationError if they were taken. Here, user can submit form without changing username or email, and that will still be valid. If I query database, I'll get ValidationError since username and email already taken. So, only run validation checks if data submitted is different than current username or email
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("That username is taken. Please choose a different one")
+        
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("That email is taken. Please choose a different one")
--- flask_blog/flask_blog/routes.py
+++ flask_blog/flask_blog/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template, url_for, flash, redirect, request # import render_template function. url_for is a function that finds exact locations of routes for us. The request object from flask lets us access query parameters (from http://localhost:5000/login?next=%2Faccount)
 from flask_blog import app, db, bcrypt
-from flask_blog.forms import RegistrationForm, LoginForm
+from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_blog.models import User, Post # fix if coupled with "from __main__ import db" in models.py
 from flask_login import login_user, logout_user, current_user, login_required
+import secrets, os
+from PIL import Image
 
 posts = [
     {
@@ -70,10 +72,42 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
-@app.route("/account")
+# Save user's image that they uploaded to our filesystem/database
+def save_picture(form_picture):
+    # We want unique image names to avoid collisions. Randomize the name of image with random hex
+    random_hex = secrets.token_hex(8) # 8 byes
+    file_name, file_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + file_ext
+    # Get root path of our app all the way to our package directory
+    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_filename)
+    
+    # We resize it in CSS to 125 px anyway, so no need to store full size image on server and take up space and slow down site
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    
+    i.save(picture_path) # saved image to filesystem
+    return picture_filename
+
+@app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
-        return render_template("account.html", title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        # picture is not a required field and does not have default preset, so check manually here:
+        if form.picture.data:
+            picture_filename = save_picture(form.picture.data)
+            current_user.image_file = picture_filename
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+        return redirect(url_for("account")) # Don't let it fall to render_template to avoid POST-GET-REDIRECT pattern. This is that popup when we refresh a page that says "Are you sure you want to reload? Data will be resubmitted". This means we will run another post request when we refresh our page. Redirecting sends a get request instead.
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    return render_template("account.html", title="Account", image_file=image_file, form=form)
 
 # Before we run app, we need to set environment variable to file we want to be our flask application. Here, export FLASK_APP=flask_blog.py
 # localhost = 127.0.0.1
